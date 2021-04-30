2c86f2b328c6f4ad90210993b55c74a18031030c
NCSA Mosaic <1239085033@qq.com>
2021-04-30 22:25:21 +0800

    利用 jsencrypt.js 插件进行加密和解密

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# rsa-encrypt" 
+# RSA 加密与解密
+
+## 辅助工具
+
+- [Vue 3](https://v3.cn.vuejs.org/)
+- [Vite](https://cn.vitejs.dev/)
+- [element-plus](https://element-plus.gitee.io/#/zh-CN)
+- [生成RSA密钥对](http://web.chacuo.net/netrsakeypair)
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,66 @@
 <template>
   <h1>{{ msg }}</h1>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div class="rsa-wrap">
+    <div class="box">
+      <el-input v-model="state.encrypt" placeholder="请输入加密内容"></el-input>
+      <el-button type="primary" @click="onEncrypt(state.encrypt)">加密</el-button>
+    </div>
+    <div class="res">{{ state.encryptRes || '---' }}</div>
+    <div class="box">
+      <el-input v-model="state.decrypt" placeholder="请输入解密内容"></el-input>
+      <el-button type="danger" @click="onDecrypt(state.decrypt)">解密</el-button>
+    </div>
+    <div class="res">结果：{{ state.decryptRes }}</div>
+  </div>
 </template>
 
 <script setup>
 import { defineProps, reactive } from 'vue'
-
+import { JSEncrypt } from 'jsencrypt'
+import { publickey, privateKey } from '../constants'
+ 
 defineProps({
   msg: String
 })
+  
+const state = reactive({ 
+  encrypt: '',
+  encryptRes: '',
+  decrypt: '',
+  decryptRes: ''
+})
 
-const state = reactive({ count: 0 })
+// 加密
+const onEncrypt = (msg) => {
+  const jsencrypt = new JSEncrypt()
+  jsencrypt.setPublicKey(publickey)
+  state.encryptRes = jsencrypt.encrypt(msg)
+}
+ 
+// 解密
+const onDecrypt = (msg) => {
+  let decrypt = new JSEncrypt()
+  decrypt.setPrivateKey(privateKey)
+  var decryptMsg = decrypt.decrypt(msg)
+  state.decryptRes = decryptMsg
+}
+
 </script>
 
 <style scoped>
-a {
-  color: #42b983;
+.rsa-wrap {
+  width: 60vw;
+  margin: 0 auto;
+}
+.box {
+  display: flex;
+  margin: .8rem 0;
+}
+.res {
+  height: 2.2rem;
+  text-align: left;
+  margin: 1rem 0 2rem;
+  padding: 0 .8rem;
+  word-break: break-all;
 }
 </style>
